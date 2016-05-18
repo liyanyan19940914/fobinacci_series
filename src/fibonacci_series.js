f2b612f94147dd9d292b42d36cd15a8346fc3b8e
liyanyan19940914 <921258351@qq.com>
2016-05-18 14:54:23 +0800

    second commit

--- src/fibonacci_series.js
+++ src/fibonacci_series.js
@@ -1,7 +1,15 @@
 'use strict';
 
 function fibonacci_series(n) {
+    var array = [0, 1];
 
+    if (n >= array.length) {
+        for (var i = array.length; i < n + 1; i++) {
+            array[i] = array[i - 1] + array[i - 2];
+        }
+    }
+
+    return array;
 }
 
 module.exports = fibonacci_series;
